3c198afcb4f01c42d344c7bdb35ce5bd4edb7918
abdullahi-auwalu-neurotech <abdullahiag123@gmail.com>
2026-09-11 13:25:55 +0100

    debug errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     _len = len(sys.argv)
     i = 1
     if (_len == 1):
-        print("0 arguments")
+        print("0 arguments.")
     elif (_len == 2):
         print("1 argument:")
         print("{}: {}".format(i, sys.argv[i]))
